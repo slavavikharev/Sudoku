d403fd95cb1d07b01450b4e7cfe310483adf588f
slavavikharev <slavavikharev@142eb7da-3426-4e83-b16f-2be5c750212a>
2015-12-18 18:29:42 +0000

    Переделал судоку
    Добавил:
    + Алгоритм
    + Классы
    + Решение с предположениями
    + Разный размер судоку
    + Произвольное решаемое судоку
    
    Пока что не добавил:
    - GUI

--- sudoku.py
+++ sudoku.py
@@ -1,106 +1,246 @@
-#/usr/bin/env python3
+import argparse
+from itertools import chain
+from random import randint
+from re import findall
+from string import ascii_uppercase, digits
 
-import sys
-
-
-def read_file(file):
-    """Reads a file"""
-    try:
-        f = open(file)
-    except FileNotFoundError:
-        print("There is no this file")
-        sys.exit(1)
-    level_str = f.read().splitlines()
-    f.close()
-    return level_str
+__all__ = ['Cell', 'Grid', 'Sudoku']
 
 
-def parse_level(level_str):
-    """
-    Takes an abstractive sudoku, 
-    returns a parsed list of lists
-    """
-    unknown = 0
-    level = []
-    for i in range(len(level_str)):
-        level.append([])
-        for j in range(len(level_str[i])):
-            if level_str[i][j] == '0':
-                level[i].append(set())
-                unknown += 1
+class Cell:
+    def __init__(self, values, x, y):
+        """
+        :type values: set
+        """
+        self.values = values
+        self.coord = (x, y)
+        self.size = len(values)
+        self.fixed = self.size == 1
+
+    def __str__(self):
+        return ''.join(self.values)
+
+    def __len__(self):
+        return self.size
+
+    def __eq__(self, other):
+        return self.values == other.values and \
+               self.coord == other.coord
+
+    def copy(self):
+        """Returns a copy of this cell"""
+        return Cell(self.values, *self.coord)
+
+    def change_values(self, values):
+        """
+        Changes values for this cell;
+        Calculates new size;
+        Determines if this cell is fixed now
+        :param values:
+        """
+        self.values = values
+        self.size = len(values)
+        self.fixed = self.size == 1
+
+
+class Grid:
+    def __init__(self, values=None):
+        """
+        :type values: str
+        """
+        if values is None:
+            return
+        values = ''.join(row.strip() for row in values.splitlines())
+        n = len(values) ** .25
+        if not n.is_integer():
+            raise ValueError("The count of elements should be square")
+        self.n = int(n) ** 2
+        if self.n == 9 or self.n == 4:
+            self.digits = digits[1:][:self.n]
+        elif self.n == 16:
+            self.digits = (digits[1:] + ascii_uppercase)[:self.n]
+        elif self.n == 25:
+            self.digits = ascii_uppercase[:self.n]
+        else:
+            raise ValueError("Only 9x9, 16x16 or 25x25 sudoku")
+        self.digits = set(self.digits)
+        self._grid = [[Cell({cell} if cell not in '.0' else self.digits, x, y)
+                       for x, cell in enumerate(row)]
+                      for y, row in enumerate(findall('.' * self.n, values))]
+
+    def __str__(self):
+        return '\n'.join(' '.join([str(cell) for cell in row])
+                         for row in self._grid)
+
+    def __getitem__(self, indexes):
+        """
+        :type indexes: tuple (int, int) or (slice, slice)
+        """
+        if all(isinstance(index, int) for index in indexes):
+            return self._grid[indexes[1]][indexes[0]]
+        return [cell for row in self._grid[indexes[1]]
+                for cell in row[indexes[0]]]
+
+    def __iter__(self):
+        return (cell for cell in self[:, :])
+
+    def get_row(self, row):
+        """Returns all cells in 'row' row
+        :param row:
+        """
+        return self[:, row:row + 1]
+
+    def get_col(self, col):
+        """Returns all cells in 'col' col
+        :param col:
+        """
+        return self[col:col + 1, :]
+
+    def get_square(self, x, y):
+        """Returns all cells in x&y's square
+        :param x:
+        :param y:
+        """
+        n = int(self.n ** .5)
+        sqr_x = x - x % n
+        sqr_y = y - y % n
+        return self[sqr_x:sqr_x + n,
+                    sqr_y:sqr_y + n]
+
+    def get_peers(self, x, y):
+        """
+        Returns all cells in col, row
+        and square for x&y's cell
+        :param x:
+        :param y:
+        """
+        units = chain(self.get_col(x),
+                      self.get_row(y),
+                      self.get_square(x, y))
+        return filter(lambda c: c != self[x, y], units)
+
+    def possible(self, x, y):
+        """Returns possible values for x&y's cell
+        :param x:
+        :param y:
+        """
+        impossible_values = {next(iter(c.values))
+                             for c in self.get_peers(x, y)
+                             if c.fixed}
+        return self.digits - impossible_values
+
+    def delete_bad(self):
+        """
+        Deletes all impossible values
+        in each cell in this grid;
+        Returns True if it's possible else False
+        """
+        for cell in (c for c in self if not c.fixed):
+            possible = self.possible(*cell.coord)
+            if not possible:
+                return False
+            self[cell.coord].change_values(possible)
+            if self[cell.coord].fixed:
+                return self.delete_bad()
+        return True
+
+    def check_correct(self):
+        """
+        Returns whether all fixed
+        cell have possible values
+        """
+        return all(next(iter(c.values))
+                   in self.possible(*c.coord)
+                   for c in self if c.fixed)
+
+    def copy(self):
+        """Returns a copy of this grid"""
+        new = Grid()
+        new.n = self.n
+        new.digits = self.digits
+        new._grid = [[cell.copy() for cell in row]
+                     for row in self._grid]
+        return new
+
+
+class Sudoku:
+    def __init__(self, values):
+        """
+        :type values: str
+        """
+        self.grid = Grid(values)
+        if not self.grid.check_correct() or \
+                not self.grid.delete_bad():
+            raise ValueError("Sudoku is not correct")
+
+    def __str__(self):
+        return str(self.grid)
+
+    def solve(self):
+        """Make this sudoku solved"""
+        self.grid = self.search(self.grid)
+
+    def search(self, grid):
+        """
+        Returns solved sudoku's grid
+        :param grid:
+        """
+        if all(c.fixed for c in grid):
+            return grid
+        c = min((cell for cell in grid if not cell.fixed), key=len)
+        for d in c.values:
+            new_grid = grid.copy()
+            new_grid[c.coord].change_values({d})
+            if not new_grid.delete_bad():
+                continue
+            solved = self.search(new_grid)
+            if solved:
+                return solved
+
+    def desolve(self):
+        """
+        Returns this unsolved
+        sudoku in string format
+        """
+        if not all(c.fixed for c in self.grid):
+            raise ValueError("Sudoku must be solved")
+        return ''.join(str(cell) if randint(0, 1) else '0'
+                       for cell in self.grid)
+
+    @staticmethod
+    def random(n):
+        """
+        Returns random generated
+        sudoku with size 'n'
+        :param n:
+        """
+        sudoku = Sudoku('0' * n ** 2)
+        sudoku.solve()
+        return sudoku.desolve()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Sudoku solver')
+    parser.add_argument_group('')
+    parser.add_argument('--file', metavar='FILE',
+                        help='solve from file', type=str)
+    parser.add_argument('--many', action='store_true',
+                        help='solve all sudoku from file')
+    parser.add_argument('--random', metavar='N', choices={4, 9, 16, 25},
+                        help='generate sudoku NxN', type=int)
+    args = parser.parse_args()
+
+    if args.file:
+        with open(args.file) as file:
+            if args.many:
+                for line in file:
+                    s = Sudoku(line)
+                    s.solve()
+                    print("%s\n" % s)
             else:
-                level[i].append(int(level_str[i][j]))
-    return level, unknown
+                s = Sudoku(file.read())
+                s.solve()
+                print(s)
 
-
-def take_column(j, level):
-    """Returns all numbers in a column"""
-    return {i[j] for i in level if type(i[j]) == int}
-
-
-def take_row(i, level):
-    """Returns all numbers in a row"""
-    return {j for j in level[i] if type(j) == int}
-
-
-def take_square(i, j, level):
-    """Returns all numbers in a square"""
-    i -= i % 3
-    j -= j % 3
-    square = set()
-    for k in range(i, i + 3):
-        for l in range(j, j + 3):
-            if type(level[k][l]) == int:
-                square.add(level[k][l])
-    return square
-
-
-def solve_sudoku(level, unknown):
-    """
-    The main function
-    takes the level and the count of unknown cells
-    returns a solved sudoku
-    """
-    last = str(level)
-    while unknown > 0:
-        for i in range(len(level)):
-            for j in range(len(level[i])):
-                if type(level[i][j]) == int:
-                    continue
-                if len(level[i][j]) == 1:
-                    level[i][j] = level[i][j].pop()
-                    unknown -= 1
-                    continue
-                level[i][j] = set()
-                for a in range(1, 10):
-                    if a in take_column(j, level) or \
-                            a in take_row(i, level) or \
-                            a in take_square(i, j, level):
-                        continue
-                    level[i][j].add(a)
-        if str(level) == last:
-            print("I can't solve it")
-            sys.exit(1)
-        last = str(level)
-    return level
-
-
-def print_result(level):
-    """Just prints the sudoku"""
-    for i in range(len(level)):
-        level[i] = [str(j) for j in level[i]]
-        if i % 3 == 0:
-            print()
-        print("".join(level[i][:3]) + ' ' +
-              "".join(level[i][3:6]) + ' ' + "".join(level[i][6:]))
-
-
-def main():
-    level_str = read_file(sys.argv[1])
-    level, unknown = parse_level(level_str)
-    result = solve_sudoku(level, unknown)
-    print_result(result)
-
-
-if __name__ == "__main__":
-    main()
+    if args.random:
+        print(Sudoku.random(args.random))
---
+++ sudoku_test.py
@@ -0,0 +1,151 @@
+import unittest
+
+from sudoku import *
+
+
+class SudokuTest(unittest.TestCase):
+    def assertCellsEqual(self, list1, list2):
+        self.assertTrue(all(c1 == c2 for c1, c2 in zip(list1, list2)))
+
+    def test_get_col(self):
+        test_sudoku = '''
+        2341
+        4123
+        1234
+        3412
+        '''
+        g = Grid(test_sudoku)
+        expected_col = [Cell({'3'}, 1, 0),
+                        Cell({'1'}, 1, 1),
+                        Cell({'2'}, 1, 2),
+                        Cell({'4'}, 1, 3)]
+        self.assertCellsEqual(g.get_col(1), expected_col)
+
+    def test_get_row(self):
+        test_sudoku = '''
+        2341
+        4123
+        1234
+        3412
+        '''
+        g = Grid(test_sudoku)
+        expected_col = [Cell({'1'}, 0, 2),
+                        Cell({'2'}, 1, 2),
+                        Cell({'3'}, 2, 2),
+                        Cell({'4'}, 3, 2)]
+        self.assertCellsEqual(g.get_row(2), expected_col)
+
+    def test_get_square(self):
+        test_sudoku = '''
+        2341
+        4123
+        1234
+        3412
+        '''
+        g = Grid(test_sudoku)
+        expected_col = [Cell({'3'}, 2, 2),
+                        Cell({'4'}, 3, 2),
+                        Cell({'1'}, 2, 3),
+                        Cell({'2'}, 3, 3)]
+        self.assertCellsEqual(g.get_square(2, 2), expected_col)
+        self.assertCellsEqual(g.get_square(3, 3), expected_col)
+        self.assertCellsEqual(g.get_square(2, 3), expected_col)
+        self.assertCellsEqual(g.get_square(3, 2), expected_col)
+
+    def test_correct_sudoku(self):
+        test_sudoku = '''
+        530070000
+        600195000
+        098000060
+        800060003
+        400803001
+        700020006
+        060000280
+        000419005
+        000080079
+        '''
+        grid = Grid(test_sudoku)
+        self.assertTrue(grid.check_correct())
+
+    def test_incorrect_sudoku(self):
+        test_sudoku = '''
+        530070005
+        600195000
+        098000060
+        800060003
+        400803001
+        700020006
+        060000280
+        000419005
+        000080079
+        '''
+        grid = Grid(test_sudoku)
+        self.assertFalse(grid.check_correct())
+
+    def test_correct_empty(self):
+        test_sudoku = '''
+        000000000
+        000000000
+        000000000
+        000000000
+        000000000
+        000000000
+        000000000
+        000000000
+        000000000
+        '''
+        grid = Grid(test_sudoku)
+        self.assertTrue(grid.check_correct())
+
+    def test_random_sudoku(self):
+        random_sudoku = Sudoku.random(9)
+        random_sudoku = Sudoku(random_sudoku)
+        random_sudoku.solve()
+        self.assertTrue(random_sudoku.grid.check_correct())
+        random_sudoku = Sudoku.random(16)
+        random_sudoku = Sudoku(random_sudoku)
+        random_sudoku.solve()
+        self.assertTrue(random_sudoku.grid.check_correct())
+
+    def test_solve_sudoku(self):
+        with open('tests.txt') as file:
+            for i, test_sudoku in enumerate(file):
+                s = Sudoku(test_sudoku)
+                s.solve()
+                self.assertTrue(s.grid.check_correct())
+
+    def test_huge_sudoku(self):
+        he = '''
+        0G0VCUB0TPR0EXOIA00Y0HKJ0
+        0S000O0KV0AH000MU00P0000N
+        00KN000R0AL00Y0C0QW0M000U
+        EYB0QM0W0HI0JU00KS0L00000
+        RAUD0GQ000000T00X0V00LWPO
+        0E0G0TF0P00YALJ0000K0X00S
+        0FR0T000BGEVKQ0S0XJC0000D
+        00SY0V0E00000RCG0P0B00OFJ
+        L0000D000SFTG0N00VO0C00HB
+        BVCJDLRM00000OHW00YAP00G0
+        VCW0PAYFH00ET00000SD0Q0OL
+        A00U00C00XYJ00V000KFNDMW0
+        H0D0JPL0K000N000G0IVF0C0E
+        0LGENSM000C00IXQ00A00V00T
+        KB0F0EW00000SH00TCMUY0PAG
+        0K00VIU00DHQ00000FTEOPLSX
+        QT00U0GP00V0RDFN000I0000A
+        IPH00X0Q0ENM00000B0J0RU00
+        G0000HTN0J0IUEAVP000W0FQ0
+        D00L0K0000OPXJ00M0QH0C0N0
+        FNEB00K0G00X000000PM0YSRW
+        00000W0UY00LH0RF0I0GV0NXP
+        U000I0PH0O0G00YD0K000FT00
+        S0000J00EF000MK0RN0Q000L0
+        0RPO0N00QMDUC0EAY0XWJI0K0
+        '''
+        s = Sudoku(he)
+        s.solve()
+        self.assertTrue(s.grid.check_correct())
+
+
+if __name__ == '__main__':
+    unittest.main()
